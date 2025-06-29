7490817186ba2541dba7b4621161cc32949f6692
Shivam8299 <shivammishra0381@gmail.com>
2025-06-29 22:15:01 +0530

    setup for my mern project

---
+++ Backend/index.js
@@ -0,0 +1,20 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+const aap = express()
+aap.use(express.json())
+
+aap.get("/",(req,res)=>{
+    res.send("server is running on port 3000")
+})
+
+const port = process.env.PORT || 4000;
+
+aap.listen(port, (error)=>{
+    if(error){
+        console.log("something went wrong", error)
+    } else {
+        console.log(`server is running on port ${port}`)
+    }
+})
